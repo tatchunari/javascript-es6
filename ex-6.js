15030e47168d8dddf3600ab400d3329709ed2c41
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-07-30 13:42:48 +0700

    feat: add answers to ex 1-7

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,9 @@
 let height = undefined;
 let result;
 
+let heightMessage = "Height is not defined";
+
+result = height ?? heightMessage;
+
+
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
